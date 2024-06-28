c2fe580d2012920424fbe38aae7eddedbb081cf3
Hiroki USUI <mench.off12@gmail.com>
2024-06-28 23:52:02 +0900

    remove class-validator

--- src/common/practices.ts
+++ src/common/practices.ts
@@ -1,59 +1,39 @@
-import { IsNumber, IsString } from 'class-validator'
-
 export class Practice {
-    @IsString()
     group_id: string
 
-    @IsString()
     date_start_place: string
 
-    @IsString()
     address: string
 
-    @IsString()
     date: string
 
-    @IsString()
     end_time: string
 
-    @IsString()
     group_name: string
 
-    @IsString()
     image?: string
 
-    @IsNumber()
     latitude?: number
 
-    @IsNumber()
     longtitude?: number
 
-    @IsString()
     name: string
 
-    @IsString()
     place: string
 
-    @IsString()
     start_time: string
 }
 
 export class CreatePracticeData {
-    @IsString()
     group_id?: string
 
-    @IsString()
     group_name?: string
 
-    @IsString()
     place?: string
 
-    @IsString()
     date?: string
 
-    @IsString()
     start_time?: string
 
-    @IsString()
     end_time?: string
 }
--- src/common/users_groups.ts
+++ src/common/users_groups.ts
@@ -1,4 +1,3 @@
-import { IsArray, IsEnum, IsObject, IsString } from 'class-validator'
 import { CreatePracticeData } from './practices.js'
 
 // 定数
@@ -33,57 +32,41 @@ export enum UserAddPracticePhase {
 }
 
 export class User {
-    @IsString()
     group_id: string
 
-    @IsString()
     user_id: string
 
-    @IsArray()
     groups: Group[]
 
-    @IsObject()
     session: UserSession | null
 
-    @IsEnum(EntityType)
     type: EntityType
 }
 
 export class Group {
-    @IsString()
     group_id: string
 
-    @IsString()
     user_id?: string
 
-    @IsString()
     group_name: string
 
-    @IsString()
     area: string
 
-    @IsEnum(EntityType)
     type?: EntityType
 }
 
 export class Relation {
-    @IsString()
     group_id: string
 
-    @IsString()
     user_id: string
 
-    @IsEnum(EntityType)
     type: EntityType
 }
 
 export class UserSession {
-    @IsEnum(UserMode)
     mode?: UserMode
 
-    @IsEnum(UserNotifyPracticesWithdrawGroupPhase || UserAddPracticePhase)
     phase?: UserNotifyPracticesWithdrawGroupPhase | UserAddPracticePhase
 
-    @IsObject()
     data?: CreatePracticeData
 }
