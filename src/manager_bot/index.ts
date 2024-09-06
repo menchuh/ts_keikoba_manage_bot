a9126e05f3ad0c4951b01e722d4fc5559a73536a
Hiroki USUI <mench.off12@gmail.com>
2024-09-07 07:01:35 +0900

    add loggers and edit deploy.yaml

--- src/manager_bot/index.ts
+++ src/manager_bot/index.ts
@@ -100,6 +100,7 @@ export const lambdaHandler = async (
 
     // イベントとユーザの取得
     const requestBody: WebhookRequestBody = JSON.parse(event.body)
+    logger.info(requestBody)
     const lineEvent = requestBody.events[0]
     logger.info(requestBody)
     logger.info(lineEvent)
